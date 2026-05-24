9ed43c2e7c6314b9eb9470034f7979cc84cda24a
Jingkun Liu <sjtu_ljk@sjtu.edu.cn>
2026-05-24 20:55:15 +0800

    Update run_train_100m.sh

--- language_models_100m/train/run_train_100m.sh
+++ language_models_100m/train/run_train_100m.sh
@@ -1,8 +1,8 @@
 env CUDA_VISIBLE_DEVICES=0,1,2,3 \
   torchrun --nproc_per_node=4 --master_port 33243 train_100m.py \
-    --data_dir ./datasets/fwe10bt \
-    --tokenizer_path ./llm/gpt2 \
-    --output_root ./models_100m \
+    --data_dir /yourpath/datasets/fwe10bt \
+    --tokenizer_path /yourpath/llm/gpt2 \
+    --output_root /yourpath/models_100m \
     --device cuda \
     --compile \
     --warmup_ratio 0.05 \
